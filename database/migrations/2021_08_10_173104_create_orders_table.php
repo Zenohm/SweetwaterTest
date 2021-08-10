ea79d91a7f0d8a87831782ee8d4d5d6fb2cc545f
Zenohm <h13rW3dOO6N0>
2021-08-10 13:50:18 -0400

    wip: add initial controller, seeder, and model for test data
    I have chosen to name the model that stores the data as Order instead of SweetwaterTest. This is based on the id being named "orderid".

---
+++ database/migrations/2021_08_10_173104_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sweetwater_test', function (Blueprint $table) {
+            $table->id("orderid");
+            $table->string("comments");
+            $table->dateTime("shipdate_expected");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sweetwater_test');
+    }
+}
